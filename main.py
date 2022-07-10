241ab8f8b6d6bd47887e55647907b481929e9ed4
Makseke <makseke@mail.ru>
2022-07-10 15:07:11 +0300

    V0.0.4
    NEW:
    -функции рандома преобразованы в одну команду
    -добавлена команда для получения информации об аккаунте

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-import discord
+import discord, os
 import random
 from discord.ext import commands
+import discord.user
+from random import randint
 
 config = {
-    'prefix': '$',
+    'prefix': '!',
 }
 
 bot = commands.Bot(command_prefix = config['prefix'])
+client = discord.Client
+chanal = discord.TextChannel
 
 @bot.command()
 async def testworking(message):
     await message.reply('Working')
 
 @bot.command()
-async def random_number(message, *arg):
-    min = int(arg[0])
-    max = int(arg[1])
-    await message.reply(random.randint(min, max))
+async def random(message, *arg):
+    if len(arg) == 2 and str(arg[0]).isdigit() == True and str(arg[1]).isdigit() == True:
+        min = int(arg[0])
+        max = int(arg[1])
+        if min > max:
+            _ = min
+            min = max
+            max = _
+        await message.reply(randint(min, max))
+    else:
+        await message.reply(arg[randint(0, len(arg))])
 
 @bot.command()
-async def random_word(message, *arg):
-    await message.reply(arg[random.randint(0, len(arg))])
+async def info(message, user: discord.User):
+    print(user.name)
+    print(user.id)
+    print(user.created_at)
 
 bot.run(config['token'])
