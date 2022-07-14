096da4a4c105bda690426d51c142d50e9f4df26d
Makseke <makseke@mail.ru>
2022-07-14 14:13:36 +0300

    V0.0.8
    NEW:
    -добавлена команда выдающая одно случайное слово из строки
    -исправлен баг когда команда рандом не срабатывала из-за не тех значений в min и max
    -добавлена команда выводящая информацию о пользователе в виде карточки
    -исправлен баг из-за которого в терминале всегда выводилось одно и тоже время вызова команд

--- bot_pycord.py
+++ bot_pycord.py
@@ -7,12 +7,14 @@ from random import randint
 
 import datetime
 
-dt = datetime.datetime.now()
-dt_string = dt.strftime("Date: %d/%m/%Y  Time: %H:%M:%S")
+def time_now():
+    dt = datetime.datetime.now()
+    dt_string = dt.strftime("Date: %d/%m/%Y  Time: %H:%M:%S")
+    return dt_string
 
 print('--------------------------------------------------')
 print('SERVER ONLINE')
-print(dt_string)
+print(time_now())
 print('--------------------------------------------------')
 
 
@@ -20,22 +22,50 @@ intents = discord.Intents.default()
 
 bot = discord.Bot(
     intents=intents,
-    debug_guilds=[995982160928444486]
+    # debug_guilds=[995982160928444486]
 )
-
 
 @bot.slash_command(description="Выдает случайное число в переданном диапазоне", name="random")
 async def makseke(message, min: Option(int), max: Option(int)):
-    print(f'RANDOM BY {message.author} / {message.guild} AT {dt_string} ')
+    print(f'RANDOM BY {message.author} / {message.guild} AT {time_now()} ')
+    if min > max:
+        _ = min
+        min = max
+        max = _
     await message.respond(randint(min, max))
+
+@bot.slash_command(description="Выдает случайное слово из введенной строки", name="random_word")
+async def makseke(message, string: Option(str)):
+    print(f'RANDOM_WORD BY {message.author} / {message.guild} AT {time_now()} ')
+    string_list = string.split()
+    if len(string_list) == 1:
+        await message.respond(string_list[0])
+    else:
+        await message.respond(string_list[randint(0, len(string_list))])
 
 @bot.slash_command(description="Выводит информацию о пользователе", name="info")
 async def info(message, member: discord.User):
-    print(f'GET INFO ABOUT {member.name} / {message.guild} BY {message.author} AT {dt_string}')
+    print(f'GET INFO ABOUT {member.name} / {message.guild} BY {message.author} AT {time_now()}')
+    dt_member = str(member.created_at).split('.')[0]
     user_info = f'Имя пользователя: {member.name}\n' \
                 f'Отображаемое имя: {member.display_name}\n' \
                 f'ID пользователя: {member.id}\n' \
-                f'Дата регистрации аккаунта: {member.created_at}\n'
+                f'Дата регистрации аккаунта: {dt_member}\n'
+    if member.name == 'makseke':
+        user_info += 'Мой повелитель... и ваш...\n'
     await message.send(user_info)
+
+@bot.slash_command(description='Выводит информацию о пользователе в виде карточки', name='info_card')
+async def info_card(message, member: discord.User):
+    print(f'GET INFO_CARD ABOUT {member.name} / {message.guild} BY {message.author} AT {time_now()}')
+    dt_member = str(member.created_at).split('.')[0]
+    embed = discord.Embed(
+        title=f'Информация о пользователе {member.name}',
+        description=f'Отображаемое имя: {member.display_name}\n' \
+                    f'ID пользователя: {member.id}\n' \
+                    f'Дата регистрации аккаунта: {dt_member}\n',
+        color=0x2EE8CA
+    )
+    await message.respond(embed=embed)
 
 bot.run(config['token'])
